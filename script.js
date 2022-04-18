3b2e6ba982b3744484f2be898221f46b1128d5da
nyinyilayhtet <nnlayhtet@gmail.com>
2022-04-17 22:32:25 -0400

    change font-seze and colors

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+h2.style.color = "yellow"
+h2.style.fontSize = "70px"
+h2.style.textAlign = "center"
+
+const p = document.createElement("p");
+p.textContent = "This paragraph added by Javascript";
+document.querySelector("body").appendChild(p);
